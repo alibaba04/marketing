fae034c3c46c9e0dbe726863822f9d492a26645c
alibaba04 <albaihaqial@gmail.com>
2021-03-18 13:31:34 +0700

    update config

--- config.php
+++ config.php
@@ -13,6 +13,7 @@ $dbPass = 'PVMMA0Akp4;(';
 $passSalt = 'UFqPNrZENKSQc5yc';
 
 //Default database link
+error_reporting(E_ALL ^ E_DEPRECATED);
 $dbLink = mysql_connect($dbHost,$dbUser,$dbPass, true)or die('Could not connect: ' . mysql_error());
 mysql_query("SET NAMES 'UTF8'");
 
