9defbdcd708b30ae955f4ce2fdcd10342dccdbc5
alibaba04 <albaihaqial@gmail.com>
2021-03-18 13:52:30 +0700

    update app name

--- index.php
+++ index.php
@@ -164,7 +164,7 @@ require_once('./function/fungsi_formatdate.php');
             <footer class="main-footer">
 
                 <div class="pull-right hidden-xs">
-                    <b>Akuntansi App</b> 2.0.0 &nbsp;&nbsp;<strong>Created by: <a href="http://instagram.com/baihaqial">alibaba</a>.
+                    <b>Marketing App</b> 2.0.0 &nbsp;&nbsp;<strong>Created by: <a href="http://instagram.com/baihaqial">alibaba</a>.
                     </div>
                     <strong>.</strong>
                     <!-- <strong>Copyright &copy; 2020 <a href="http://instagram.com/baihaqial">alibaba's</a>.</strong> All rights reserved.  -->
