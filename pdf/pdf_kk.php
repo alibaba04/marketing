0d049ad647b8834afe86b908aa501f55d428904f
alibaba04 <albaihaqial@gmail.com>
2021-05-25 11:14:48 +0700

    update kk

--- pdf/pdf_kk.php
+++ pdf/pdf_kk.php
@@ -1076,9 +1076,9 @@ $pdf->SetTextColor(0);
 $pdf->SetDrawColor(0);
 $pdf->SetFont('helvetica', 'bu', 11);
 $pdf->Cell(190,6,'DESAIN KUBAH',0,1,'C',0);
-/*if ($hasil['filekubah']!='') {
+if ($hasil['filekubah']!='') {
   $pdf->image('../../uploads/'.$hasil['filekubah'],12,70,190,143);
-}*/
+}
 $pdf->SetMargins(13, 10, 10, true);
 $pdf->Ln(170);
 $pdf->SetFont('helvetica', 'b', 14);
@@ -1147,9 +1147,9 @@ $pdf->SetTextColor(0);
 $pdf->SetDrawColor(0);
 $pdf->SetFont('helvetica', 'bu', 11);
 $pdf->Cell(190,6,'DESAIN KALIGRAFI',0,1,'C',0);
-/*if ($hasil['filekaligrafi']!='') {
+if ($hasil['filekaligrafi']!='') {
    $pdf->image('../../uploads/'.$hasil['filekaligrafi'],12,70,190,143);
-}*/
+}
 $pdf->SetMargins(13, 10, 10, true);
 $pdf->Ln(250);
 $pdf->SetTextColor(130);
